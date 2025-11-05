80ad2486ee733ad31ce2121facea87aa77f7ae5b
tamaro <tamarowalter@yahoo.de>
2025-11-05 20:22:21 +0100

    add first photos

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,19 @@
+import { Button, Container, Image, IconButton } from "@chakra-ui/react"
+import { FaCircleArrowRight } from "react-icons/fa6";
+import bandfoto_1 from '../data/pix/bandfoto_1.jpg';
+
+interface Photo {
+  id: number;
+  source: string;
+  alt: string;
+}
+
 export default function About() {
-    return (
-        <></>
-    );
+  return (
+    <Image height="30rem" fit="contain" rounded="md" src={bandfoto_1} alt="We are Current Street">
+      <IconButton>
+        <FaCircleArrowRight />
+      </IconButton>
+    </Image >
+  );
 }
