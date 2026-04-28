39c90461b3735ff8b5e432780c4a6191f00382e8
Tamaro Walter <tamaro.walter@uni-muenster.de>
2026-04-29 00:26:55 +0200

    Improve/about (#5)
    * use uniform color scheme
    
    * add members to about site
    
    * change about site structure, add overlay to carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 // Imports from react and UI library.
 import { BrowserRouter, Routes, Route, Link, useLocation, useNavigate} from 'react-router-dom';
-import { Button, Flex, Box, Text,  Container, HStack, Link as ChakraLink, Icon, Stack, Drawer, IconButton, Portal, CloseButton, Spacer, VStack} from '@chakra-ui/react';
+import { Button, Flex, Box, Text,  Container, HStack, Link as ChakraLink, Icon, Stack, Drawer, IconButton, Portal, Spacer, VStack} from '@chakra-ui/react';
 import { useState } from 'react';
 // Import core.
-import { getIcon, iconMap } from './core/lib';
+import { getIcon } from './core/lib';
+import { iconMap, colors } from './core/theme';
 import { useLanguage, useTranslation } from './core/LanguageContext';
 // Import components styling.
 import './App.css';
@@ -43,7 +44,7 @@ export default function App() {
 
 function AppHeader() {
   return (
-    <Box as="header" className="header" mt="1rem">
+    <Box as="header" className="header" pt={{ base: "1.2rem", md:"0" }} bg="rgba(50, 70, 65, .4)">
       <Box display={{ base: "flex", md: "none" }}>
         <MobileNav/>
       </Box>
@@ -70,20 +71,25 @@ function MobileNav() {
         <Portal>
           <Drawer.Backdrop/>
           <Drawer.Positioner>
-            <Drawer.Content bg="#4a6660" color="#f4f4f5"
+            <Drawer.Content bg={colors.bg}
               _closed={{ animationDuration: "0.4s", animationTimingFunction: "ease-in"}}
               _open={{animationDuration: "0.5s",}}
             >
-              <Drawer.Header/>
+              <Drawer.Header mt="0.2rem">
+                <Drawer.Title>
+                  <Text pt="1" color={colors.text} fontWeight="bold" fontSize="2xl">CURRENT STREET</Text>
+                </Drawer.Title>
+              </Drawer.Header>
               <Drawer.Body display="flex" flexDirection="column" h="100%">
                 <VStack pt="2rem">
                   {navigation.map(({route, text}) => (
                     <Button
+                    borderRadius="xl" borderWidth="2px" borderColor={colors.text}
                     w="100%"
                     size="2xl"
-                    _active={{ transform: 'scale(0.95)', opacity: 0.4, bg: '#a1aaaa' }}
-                    _hover={{bg: '#a1aaaa'}}
-                    backgroundColor={(route == location.pathname) ? '#a1aaaa' : '#f4f4f5'}
+                    _active={{ transform: 'scale(0.95)', opacity: 0.4, bg: colors.accentgreen }}
+                    _hover={{bg: colors.accentgreen}}
+                    backgroundColor={(route == location.pathname) ? colors.accentgreen : colors.accentwhite}
                     onClick={() => {
                       setOpen(false);
                       navigate(route);
@@ -93,8 +99,8 @@ function MobileNav() {
                   ))}
                 </VStack>
                 <Spacer/>
-                <VStack bg="#a1aaaa" borderRadius="2xl" borderColor="#1c1c1e" borderWidth="2px">
-                  <Text pt="1.2rem" pb="0.2rem" fontSize="lg" fontWeight="bold" color="#1c1c1e">{getString('social_media')}</Text>
+                <VStack bg={colors.accentgreen} borderRadius="2xl" borderColor={colors.border} borderWidth="2px">
+                  <Text pt="1.2rem" pb="0.2rem" fontSize="lg" fontWeight="bold" color={colors.text}>{getString('social_media')}</Text>
                   <Spacer/>
                   <HStack pt="0.2rem" pb="1.2rem" gap="4" >
                   {social.filter(({icon}) => ['instagram', 'youtube', 'spotify', 'apple'].includes(icon)).map(({ i, href, icon }) => (
@@ -111,7 +117,9 @@ function MobileNav() {
                 </Button>
               </Drawer.Footer>
               <Drawer.CloseTrigger asChild>
-                <CloseButton size="2xl" />
+                <Icon size="2xl" me="1rem" pe="auto" cursor="pointer" mt="1rem" color={colors.text}>
+                  {getIcon('close')}
+                </Icon>
               </Drawer.CloseTrigger>  
             </Drawer.Content>
           </Drawer.Positioner>
@@ -128,7 +136,7 @@ function DesktopNav() {
     <Container className="header-child">
       <Flex gap="2" className="background-box">
         {navigation.map(({route, text}) => (
-          <Button backgroundColor={(route == location.pathname) ? '#a1aaaa' : '#f4f4f5'} asChild>
+          <Button backgroundColor={(route == location.pathname) ? colors.accentgreen : colors.accentwhite} asChild>
             <Link className="navigation-item" to={route} >{getString(text)}</Link>
           </Button>
           ))}
@@ -142,29 +150,27 @@ function AppFooter() {
   const { language, setLanguage } = useLanguage();
   return (
     <Container as="footer" className="footer" display={{base: "none", md: "block"}}>
-      <Stack gap="6">
-        <Stack direction="row" justify="space-between" align="center">
-          <HStack gap="4">
-            {social.map(({ i, href, icon }) => (
-              <ChakraLink className="footer-social" key={i} href={href} colorPalette="gray" target="_blank" rel="noopener noreferrer">
-                <Icon size="lg">{getIcon(icon as keyof typeof iconMap)}</Icon>
-              </ChakraLink>
-            ))}
-          </HStack>
-          <Button
-            display={{ base: "none", md: "block" }}
-            variant="plain"
-            className="footer-togglelang"
-            onClick={() => setLanguage(language === 'en' ? 'de' : 'en')}
-          >
-            {getString("lang_toggle")}
-          </Button>
-        </Stack>
+      <Stack direction="row" justify="space-between" align="center">
+        <HStack gap="4">
+          {social.map(({ i, href, icon }) => (
+            <ChakraLink className="footer-social" key={i} href={href} colorPalette="gray" target="_blank" rel="noopener noreferrer">
+              <Icon size="lg">{getIcon(icon as keyof typeof iconMap)}</Icon>
+            </ChakraLink>
+          ))}
+        </HStack>
         <Flex align="center" justify="center" width="100%">
           <Text className="footer_copyright">
             © {new Date().getFullYear()} {getString("copyright")}
           </Text>
         </Flex>
+        <Button
+          display={{ base: "none", md: "block" }}
+          variant="plain"
+          className="footer-togglelang"
+          onClick={() => setLanguage(language === 'en' ? 'de' : 'en')}
+        >
+          {getString("lang_toggle")}
+        </Button>
       </Stack>
     </Container>
   );
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,114 +1,196 @@
-import type { IconButtonProps } from "@chakra-ui/react"
-import { AspectRatio, Box, Carousel, Container, IconButton, Image, Blockquote } from "@chakra-ui/react"
-import { forwardRef } from "react"
-import { getIcon} from '../core/lib';
+import { Flex, Stack, VStack, Avatar, AspectRatio, Box, Carousel, Container, IconButton, Image, Text, Card } from "@chakra-ui/react"
+import { forwardRef, useState, useEffect } from "react"
+import { createPortal } from 'react-dom';
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import { getIcon } from '../core/lib';
+import { colors } from "../core/theme";
 import { useTranslation } from '../core/LanguageContext';
 import photos from "../data/photos.json";
-
+import members from "../data/members.json";
 
 export default function About() {
+  const { getString } = useTranslation();
   return (
-    <Container minH="30rem" h="100%" w="100%">
-      <SlideShow/>
-      <BandDescription/>
+    <Container>
+      <VStack gap="5">
+        {/** Story card*/}
+        <Card.Root w="100%" borderRadius="2xl">
+          <Card.Body gap="2">
+            <Card.Title>{getString("band_description_header")}</Card.Title>
+            <Card.Description>
+              <Text fontSize="md">{getString("band_description")}</Text>
+            </Card.Description>
+          </Card.Body>
+        </Card.Root>
+
+        {/** Photos*/}
+        <Card.Root w="100%" borderRadius="2xl">
+          <Card.Body gap="2">
+            <Card.Title>{getString("photos")}</Card.Title>
+            <Card.Description>
+              <SlideShow/>
+            </Card.Description>
+          </Card.Body>
+        </Card.Root>
+
+        {/** The Band */}
+        <Card.Root w="100%" borderRadius="2xl">
+          <Card.Body gap="2">
+            <Card.Title>{getString("the_band")}</Card.Title>
+            <Card.Description ps="2rem" pe="2rem">
+              <Stack direction={{base: "column", md: "row"}} w="100%" gap="4">
+                {members.map((member) => (
+                    <MemberCard
+                      key={member.id}
+                      id={member.id}
+                      name={member.name}
+                      instruments={member.instruments}
+                      desc={member.desc}
+                      photo_id={member.photo_id}
+                    />
+                ))}
+              </Stack>
+            </Card.Description>
+          </Card.Body>
+        </Card.Root>
+
+        {/** Booking 
+        <Card.Root w="100%" borderRadius="2xl">
+          <Card.Body gap="2">
+            <Card.Title>{getString("the_band")}</Card.Title>
+            <Card.Description>
+              E-mail form
+            </Card.Description>
+          </Card.Body>
+        </Card.Root>
+        */}
+      </VStack>
     </Container>
   );
 }
 
 const SlideShow = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (!isOpen) return;
+    document.body.style.overflow = 'hidden'; // Setup.
+    return () => {document.body.style.overflow = ''}; // Cleanup.
+  }, [isOpen]);
+
+  useEffect(() => {
+    if (!isOpen) return; 
+    const handleKeyDown = (e: KeyboardEvent) => {if (e.key === 'Escape') setIsOpen(false);};
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {window.removeEventListener('keydown', handleKeyDown)};
+  }, [isOpen]);
+
   return (
-     <Carousel.Root
-        slideCount={photos.length}
-        maxW="2xl"
-        mx="auto"
-        gap="4"
-        position="relative"
-        colorPalette="white"
-    >
-        <Carousel.Control gap="4" width="full" position="relative">
+    <>
+    {/*render the carousel as default, that on click turns into an overlay. pointer cursor indicates that it's clickable*/}
+    <PhotoCarousel onImageClick={() => setIsOpen(true)} />
+
+    {/*Render the overlay as portal with the document body in the background when the state is open */}
+    {isOpen && createPortal(
+      <Box position="fixed" top="0" left="0" right="0" bottom="0" bg="blackAlpha.800"
+        zIndex="9999" display="flex" alignItems="center" justifyContent="center" 
+        onClick={() => setIsOpen(false)}>
+        <Box w={{base: "100%", md:"70%"}}
+            onClick={(e) => e.stopPropagation()}>
+          <PhotoCarousel isZoomable/>
+        </Box>
+      </Box>,
+      document.body
+    )}
+    </>
+    
+  );
+}
+
+const PhotoCarousel = ({ isZoomable = false, onImageClick}: PhotoCarouselProps) => {
+  return (
+    <Carousel.Root slideCount={photos.length} w="100%" mx="auto">
+      <Carousel.ItemGroup>
+        {photos.filter(photo => photo.name?.startsWith('bandfoto_')).map((photo, index) => (
+          <Carousel.Item key={photo.id} index={index}>
+            <AspectRatio ratio={16 / 9} maxH="60vh" w="full">
+              {isZoomable ? (
+                <Box w="100%" h="80vh" overflow="hidden" bg="black">
+                  <TransformWrapper initialScale={1} minScale={1} maxScale={4} centerOnInit={true}>
+                    <TransformComponent
+                      wrapperStyle={{ width: '100%', height: '100%' }}
+                      contentStyle={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                    >
+                      <Image src={photo.source} alt={photo.alt} maxH="100%" maxW="100%" objectFit="contain"/>
+                    </TransformComponent>
+                  </TransformWrapper>
+                </Box>
+              ) : (
+                <AspectRatio ratio={16 / 9} maxH="60vh" w="full">
+                  <Box display="flex" alignItems="center" justifyContent="center">
+                    <Image src={photo.source} alt={photo.alt} objectFit="cover" maxH="100%" maxW="100%" cursor={onImageClick ? "pointer" : "default"}
+                    onClick={onImageClick}/>
+                  </Box>
+                </AspectRatio>
+              )}
+            </AspectRatio>
+          </Carousel.Item>
+        ))}
+      </Carousel.ItemGroup>
+
+      <Carousel.Control justifyContent="center" gap="4">
         <Carousel.PrevTrigger asChild>
-            <ActionButton insetStart="4">
-              {getIcon('arrowleft')}
-            </ActionButton>
+          <IconButton size="xs" variant="ghost">
+            {getIcon('arrowleft')}
+          </IconButton>
         </Carousel.PrevTrigger>
 
-        <Carousel.ItemGroup width="full">
-            {photos.map((photo) => (
-             <PhotoImage key={photo.id} id={photo.id} source={photo.source} alt={photo.alt}/>
-            ))}
-        </Carousel.ItemGroup>
+        <Carousel.Indicators />
 
         <Carousel.NextTrigger asChild>
-            <ActionButton insetEnd="4">
-              {getIcon('arrowright')}
-            </ActionButton>
+          <IconButton size="xs" variant="ghost">
+            {getIcon('arrowright')}
+          </IconButton>
         </Carousel.NextTrigger>
-
-        <Box position="absolute" bottom="6" width="full">
-            <Carousel.Indicators
-            transition="width 0.2s ease-in-out"
-            transformOrigin="center"
-            opacity="0.5"
-            boxSize="2"
-            _current={{ width: "10", bg: "colorPalette.subtle", opacity: 1 }}
-            />
-        </Box>
-        </Carousel.Control>
+      </Carousel.Control>
     </Carousel.Root>
   );
-
 }
 
-const BandDescription = () => {
-  const { getString } = useTranslation();
+const MemberCard = ({id, name, instruments, desc, photo_id} : Member) => {
+  const {getString} = useTranslation();
+  const photo = photos.find(e => e.id == photo_id);
+
   return (
-    <Blockquote.Root className="background-box" mt="2rem" justify="center">
-      <Blockquote.Content whiteSpace="pre-line">
-        {getString("currentstreet_description")}
-      </Blockquote.Content>
-    </Blockquote.Root>
+    <Card.Root key={id} w={{base: "100%", md: "20%"}} h={{base: "20%", md:"100%"}}>
+      <Card.Body py="3" gap="4">
+        <Card.Title w="100%" display="flex" justifyContent="center" alignItems="flex-end">
+          <Avatar.Root bg={colors.accentgreen}>
+            <Avatar.Image src="" />
+            <Avatar.Fallback name={name}/>
+          </Avatar.Root>
+        </Card.Title>
+        <Card.Description display="flex" justifyContent="center">
+          <VStack>
+            <Text color={colors.text} fontSize="sm" fontWeight="bold">{name}</Text>
+            <Text color={colors.text} fontSize="xs">{instruments.map((instrument) => getString(instrument)).join(", ")}</Text>
+            <Text color={colors.text} fontSize="xs">{desc}</Text>
+          </VStack>
+        </Card.Description>
+      </Card.Body>
+    </Card.Root>
   );
 }
 
-// Single Components.
-const PhotoImage = ({ id, source, alt }: Photo) => {
-  return (
-    <Carousel.Item key={id} index={id}>
-      <AspectRatio ratio={16 / 9} maxH="72vh" w="full">
-        <Box display="flex" alignItems="center" justifyContent="center">
-          <Image
-            src={source}
-            alt={alt}
-            objectFit="contain"
-            maxH="100%"
-            maxW="100%"
-          />
-        </Box>
-      </AspectRatio>
-    </Carousel.Item>
-  )
+interface Member {
+  id: number;
+  name: string;
+  instruments: string[];
+  desc: string;
+  photo_id: number;
 }
 
-const ActionButton = forwardRef<HTMLButtonElement, IconButtonProps>(
-  function ActionButton(props, ref) {
-    return (
-      <IconButton
-        {...props}
-        ref={ref}
-        size="xs"
-        variant="outline"
-        rounded="full"
-        position="absolute"
-        zIndex="1"
-        bg="bg"
-      />
-    )
-  },
-)
-
-// Interfaces and Data objects.
-interface Photo {
-  id: number;
-  source: string;
-  alt: string;
+interface PhotoCarouselProps {
+  isZoomable?: boolean;
+   onImageClick?: () => void;
 }
